055a0e4170e85300558beedea9ed049d9309ca7c
Mdidu <alexandre.meddas@gmail.com>
2021-09-06 09:14:00 +0200

    updated route & user-info-details

--- src/main/webapp/app/core/context/global-variables.ts
+++ src/main/webapp/app/core/context/global-variables.ts
@@ -3,10 +3,11 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class GlobalVariables {
 
-  personalSubbarBtns: Array<{logo: string, txt: string}> = [{logo: '', txt:'Détails'}, {logo: '', txt:'Authentification'}, {logo: '', txt:'Mot de passe'}, {logo: '', txt:'Carte de crédit'}, {logo: '', txt:'Adresse'}, {logo: '', txt:'Projets'}, {logo: '', txt:'Communautés'}];
+  login: string = localStorage.getItem('user') as string;
+  personalSubbarBtns: Array<{path: string, logo: string, txt: string}> = [{path: `details/${this.login}/view`, logo: '', txt:'Détails'}, {path: 'authentication', logo: '', txt:'Authentification'}, {path: 'password', logo: '', txt:'Mot de passe'}, {path: 'creditCard', logo: '', txt:'Carte de crédit'}, {path: 'address', logo: '', txt:'Adresse'}, {path: 'project', logo: '', txt:'Projets'}, {path: 'community', logo: '', txt:'Communautés'}];
 
-  communitySubbarBtns: Array<{logo: string, txt: string}> = [{logo: '', txt:'Settings'}, {logo: '', txt:'Member request'}, {logo: '', txt:'Members'}];
+  communitySubbarBtns: Array<{path: string, logo: string, txt: string}> = [{path: 'settings', logo: '', txt:'Settings'}, {path: 'memberRequest', logo: '', txt:'Member request'}, {path: 'members', logo: '', txt:'Members'}];
 
-  projectSubbarBtns: Array<{logo: string, txt: string}> = [{logo: '', txt:'Settings'}, {logo: '', txt:'Description'}, {logo: '', txt:'Budget'}, {logo: '', txt:'Account'}, {logo: '', txt:'Rewards'}];
+  projectSubbarBtns: Array<{path: string, logo: string, txt: string}> = [{path: 'settings', logo: '', txt:'Settings'}, {path: 'description', logo: '', txt:'Description'}, {path: 'budget', logo: '', txt:'Budget'}, {path: 'account', logo: '', txt:'Account'}, {path: 'reward', logo: '', txt:'Rewards'}];
 }
 export default GlobalVariables;
